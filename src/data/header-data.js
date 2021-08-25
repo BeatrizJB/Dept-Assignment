bdc333ba898b09481fa99975cd45225509e6d23c
Beatriz JB <bjbilrero@gmail.com>
2021-08-25 12:15:20 +0100

    created some components files + started with creating fake json

---
+++ src/data/header-data.js
@@ -0,0 +1,116 @@
+import headerImage from "../assets/images/Header.png"
+
+export const headerData = {
+  id: 1,
+  title: "Work",
+  logo: "Dept",
+  button: "View Case",
+  header: headerImage,
+  alt: "Man with his back turned, wearing a backpack, staring pensively at a wall covered in papers",
+  menu: [
+    {
+      id: 1,
+      name: "Home",
+      url: "/",
+    },
+    {
+      id: 2,
+      name: "Werk",
+      url: "/",
+    },
+    {
+      id: 3,
+      name: "Over",
+      url: "/",
+    },
+    {
+      id: 4,
+      name: "Diensten",
+      url: "/",
+    },
+    {
+      id: 5,
+      name: "Partners",
+      url: "/",
+    },
+    {
+      id: 6,
+      name: "Stories",
+      url: "/",
+    },
+    {
+      id: 7,
+      name: "Vacatures",
+      url: "/",
+    },
+    {
+      id: 8,
+      name: "Events",
+      url: "/",
+    },
+    {
+      id: 9,
+      name: "Contacts",
+      url: "/",
+    },
+  ],
+  landen: [
+    {
+      id: 1,
+      name: "Global",
+      url: "/",
+    },
+    {
+      id: 2,
+      name: "Nederland",
+      url: "/",
+    },
+    {
+      id: 3,
+      name: "United States",
+      url: "/",
+    },
+    {
+      id: 4,
+      name: "Ireland",
+      url: "/",
+    },
+    {
+      id: 5,
+      name: "United Kingdom",
+      url: "/",
+    },
+    {
+      id: 6,
+      name: "Deutschland",
+      url: "/",
+    },
+    {
+      id: 7,
+      name: "Schweiz",
+      url: "/",
+    },
+  ],
+  socialMedia: [
+    {
+      id: 1,
+      name: "Facebook",
+      url: "https://www.facebook.com/DeptAgency",
+    },
+    {
+      id: 2,
+      name: "Twitter",
+      url: "https://twitter.com/deptagency?lang=en",
+    },
+    {
+      id: 3,
+      name: "Instagram",
+      url: "https://www.instagram.com/deptagency/?hl=en",
+    },
+    {
+      id: 4,
+      name: "LinkedIn",
+      url: "https://www.linkedin.com/company/deptagency/",
+    },
+  ],
+};
---
+++ src/data/work-data.js
@@ -0,0 +1,217 @@
+import bolcom from "../assets/images/bolcom.png";
+import kempen from "../assets/images/kempen.png";
+import philips from "../assets/images/philips.png";
+import museum from "../assets/images/gemeentemuseum.png";
+import florensis from "../assets/images/florensis.png";
+import lightning from "../assets/images/lightning.png";
+import tui from "../assets/images/tui.png";
+import chocomel from "../assets/images/chocomel.png";
+import jbl from "../assets/images/jbl.png";
+import zalando from "../assets/images/zalando.png";
+import koninklijke from "../assets/images/koninklijke-bibliotheek.png";
+import liberty from "../assets/images/liberty-global.png";
+import arla from "../assets/images/arla.png";
+
+export const worksFilter = [
+  {
+    id: 1,
+    name: "all work",
+    value: "all-work",
+  },
+];
+
+export const industriesFilter = [
+  {
+    id: 1,
+    name: "all industries",
+    value: "all-industries",
+  },
+];
+
+/*  {
+    id: 2,
+    name: "digital teams",
+    value: "digital-teams",
+  },
+  {
+    id: 3,
+    name: "strategy",
+    value: "strategy",
+  },
+  {
+    id: 4,
+    name: "Diensten",
+    value: "/",
+  },
+  {
+    id: 5,
+    name: "Partners",
+    value: "/",
+  },
+  {
+    id: 6,
+    name: "Stories",
+    value: "/",
+  },
+  {
+    id: 7,
+    name: "Vacatures",
+    value: "/",
+  },
+  {
+    id: 8,
+    name: "Events",
+    value: "/",
+  },
+  {
+    id: 9,
+    name: "Contacts",
+    value: "/",
+  },
+  {
+    id: 10,
+    name: "Home",
+    value: "/",
+  },
+  {
+    id: 11,
+    name: "Werk",
+    value: "/",
+  },
+  {
+    id: 12,
+    name: "Over",
+    value: "/",
+  },
+  {
+    id: 13,
+    name: "Diensten",
+    value: "/",
+  }, */
+
+export const worksInfo = [
+  {
+    id: 1,
+    image: bolcom,
+    alt: "A group of people enjoying a waterslide on the deck of a lake",
+    name: "BOL.COM",
+    title: "A summer island in the Netherlands",
+  },
+  {
+    id: 2,
+    image: kempen,
+    alt: "Three people sitting and discussing the papers they hold",
+    name: "KEMPEN",
+    title: "A 100 years of Mondrian and Dje Stijl",
+  },
+  {
+    id: 3,
+    image: philips,
+    alt: "4 ceiling lamps and 2 desk lamps, all lit and with different shapes",
+    name: "PHILIPS",
+    title: "Beautiful design meets innovative technology",
+  },
+  {
+    id: 4,
+    image: museum,
+    alt: "A woman staring at a Mondrian paiting with her back turned",
+    name: "GEMEENTEMUSEUM",
+    title: "A 100 years of Mondrian and Dje Stijl",
+  },
+  {
+    id: 5,
+    image: florensis,
+    alt: "Man with his back turned, wearing a backpack, staring pensively at a wall covered in papers",
+    name: "Florensis",
+    title: "Rethinking the entire online ecosystem",
+  },
+  {
+    id: 6,
+    image: "none",
+    name: "MICROSOFT",
+    title:
+      "Tapping into Ireland's unique gaming culture and bringing a fresh flavour to their Xbox social media channels",
+  },
+  {
+    id: 7,
+    image: "none",
+    name: "O'NEIL",
+    title: "Integrating existing content into O'Neil's new e-commerce platform",
+  },
+  {
+    id: 8,
+    image: lightning,
+    alt: "A city lamppost with building in the background",
+    company: "BE LIGHTNING",
+    title: "Delivering clarity on a global scale",
+  },
+  {
+    id: 9,
+    image: tui,
+    alt: "An ilustration of a confused tourist holding a map",
+    company: "TUI",
+    title: "Swipe to find your next holiday destination",
+  },
+  {
+    id: 10,
+    image: "none",
+    company: "BUTLINS",
+    title:
+      "Enhancing customer experience for personalised holiday planning using real time data",
+  },
+  {
+    id: 11,
+    image: "none",
+    company: "VACANSESELECT",
+    title:
+      "Predicting booking behaviour for holidays with smart data, voice search and machine learning",
+  },
+  {
+    id: 12,
+    image: chocomel,
+    alt: "Various packages of the chocomel drink",
+    company: "CHOCOMEL",
+    title: "A campaign for the limited edition letter packs",
+  },
+  {
+    id: 13,
+    image: jbl,
+    alt: "A man with JBL headphones on",
+    company: "JBL",
+    title: "Live like a champion with Jerome Booteng",
+  },
+  {
+    id: 14,
+    image: zalando,
+    alt: "Two men working in front of a whiteboard",
+    company: "ZALANDO",
+    title: "Innovative SEO and content strategy for Zalando",
+  },
+  {
+    id: 15,
+    image: koninklijke,
+    alt: "A pile of books with only the spines showing, in a red backgroud",
+    company: "KONINKLIJKE BIBLIOTHEEK",
+    title: "the search for the most influential book ever",
+  },
+  {
+    id: 16,
+    image: liberty,
+    alt: "A side shot of many computer servers",
+    company: "LIBERTY GLOBAL",
+    title: "Delivering complex commerce solutions",
+  },
+  {
+    id: 17,
+    image: arla,
+    alt: "An assortment of protein drinks and yogurts",
+    company: "ARLA",
+    title: "Swipe to find your next holiday destination",
+  },
+];
+
+export const testemonial = {
+  id: 1,
+  text: "Dept helped us tell our story through a bold new identity and a robust online experience. To the tune of 60% growth in online bookings in just one month.",
+  author: "MATTIJS TEN BRINK - CEO, TRANSAVIA",
+};
